4605f21b9f1cd6d4040aa439f6f0fa1433b5d492
emilyrh <114513651+emilyrh@users.noreply.github.com>
2025-05-15 12:27:21 +0100

    Improved user experience

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function displaySuperpower(response) {
-  console.log("outcome generated");
   new Typewriter("#superpower", {
     strings: response.data.answer,
     autoStart: true,
@@ -15,10 +14,12 @@ function generateSuperpower(event) {
   let apiKey = "a12670296b5b7ac064111tf40b315o03";
   let prompt = `User instructions: Generate one funny yet frustrating caveat for the superpower of ${instructionsInput.value}`;
   let context =
-    "You are an expert on superheroes with a creative sense of humour. Your mission is to grant the user the superpower of their choosing, but generate one funny yet frustrating caveat. Use basic HTML. Make sure to follow the user instructions. Please start by saying congratulating the user on their new superpower in bold font, add a </br> before and after the part where you explain the caveat, and finally write a funny comment about how you hope they're happy with their choice.";
+    "You are an expert on superheroes with a creative sense of humour. Your mission is to grant the user the superpower of their choosing with one funny yet frustrating caveat, in basic HTML and separate each section with a </br>. Make sure to follow the user instructions. Please start by saying congratulations to the user on their new superpower in a <strong> element, then explain the caveat, and finally write a funny comment about how you hope they're happy with their choice.";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("generating outcome");
+  let superpowerElement = document.querySelector("#superpower");
+  superpowerElement.classList.remove("hidden");
+  superpowerElement.innerHTML = `<div class="generating">Please wait while we grant you the power of ${instructionsInput.value}...</div>`;
 
   axios.get(apiURL).then(displaySuperpower);
 }
